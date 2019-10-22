7d1dfa22803e4dcaca0fa6356f3d9905c2efca3d
cleverKai <951040436@qq.com>
2019-10-22 16:06:02 +0800

    add createComponents

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import moveRoute from './move'
+import mineRoute from './mine'
+import cinemaRoute from './cinema'
 
 Vue.use(VueRouter)
 
 const routes = [
+  moveRoute,
+  mineRoute,
+  cinemaRoute,
+    // 路由重定向，当路由匹配不到组件时，自动加载move
   {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path:"/*",
+    redirect:"/move"
   }
 ]
 
---
+++ src/views/mine/Mine.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Header title="我的喵喵"></Header>
+        <div id="content">
+            <Login></Login>
+        </div>
+        <TabBar></TabBar>
+    </div>
+</template>
+
+<script>
+    import Header from '@/components/header/Header'
+    import TabBar from '@/components/tabbar/TabBar'
+    import Login from '@/components/login/Login'
+    export default {
+        name: "Mine",
+        components:{
+            Header,
+            TabBar,
+            Login
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
